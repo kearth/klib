c6e3a51bcc4089e1ca9b86d5f6e6fc276e7b43e2
jiakun01 <jiakun01@baidudeMacBook-Pro.local>
2020-11-12 20:43:03 +0800

    add

--- index.php
+++
@@ -1,2 +0,0 @@
-<?php 
-require(__dir__ . '/vendor/autoload.php');
--- src/jsonFormat.php
+++ src/jsonFormat.php
@@ -1,9 +1,23 @@
 <?php 
-namespace Ktools
+namespace Ktools;
 
 class JsonFormat {
     public static function checkFormat(string $jsonStr) {
-        $result = json_decode($jsonStr);
-        return $result;
+        try{
+            $result = json_decode($jsonStr, true, 512, JSON_UNESCAPED_UNICODE);
+            if(is_null($result)) {
+                return json_last_error_msg(); 
+            } 
+            return $jsonStr;
+        
+        } catch(\JsonException $jsonException) {
+            var_export($jsonException);
+            return $jsonException->getMessage();
+        }
+        return 5;
+    }
+
+    public static function parser() {
+    
     }
 }
