7af9fffab2edbaf3dcf9be7c27c0324d3445f1eb
kearth <kearth@yeah.net>
2023-09-28 11:12:33 +0800

    20230928

--- main.go
+++ main.go
@@ -2,19 +2,48 @@ package main
 
 import (
 	"fmt"
-
-	"time"
-
-	"github.com/kearth/kgolib/ktime"
+	"sync"
+	"sync/atomic"
 )
 
+type Customer struct {
+	mutex sync.RWMutex
+	id    string
+
+	age int
+}
+
 func main() {
-	//	kf := ktime.PHPLayout("Y-m-d H:i:s N")
-	//	var t1 time.Time
-	fmt.Printf("%+v\n", ktime.PHPFormat(time.Now().AddDate(0, 0, 0), "Y-m-d H:i:s  t"))
-	//	fmt.Printf("%+v\n", ktime.PHPFormat(time.Now().AddDate(+9000, 0, 0), "Y-m-d H:i:s  X"))
-	//	fmt.Printf("%+v\n", ktime.PHPFormat(time.Now().AddDate(-2000, 0, 0), "Y-m-d H:i:s  X"))
-	//	a, b := t1.ISOWeek()
-	loc, _ := time.LoadLocation("Europe/Berlin")
-	fmt.Printf("%+v", time.Now().In(loc).Location())
+	wg := sync.WaitGroup{}
+	var v uint64
+
+	for i := 0; i < 3; i++ {
+		go func() {
+			wg.Add(1)
+			atomic.AddUint64(&v, 1)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(v)
+}
+
+func (c *Customer) UpdateAge(age int) error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if age < 0 {
+		return fmt.Errorf("age should be positive for customer %v", c)
+	}
+
+	c.age = age
+	return nil
+}
+
+func (c *Customer) String() string {
+	fmt.Println("enter string method")
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	return fmt.Sprintf("id %s, age %d", c.id, c.age)
 }
